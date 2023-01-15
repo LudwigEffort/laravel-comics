68fd99012bd6df4cfc2858cbecd2efd6d99aa4b9
LudwigEffort <dr.feynman65@gmail.com>
2023-01-16 00:01:50 +0100

    add first layout draft

---
+++ config/menu.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    [
+        'route' => ['home'],
+        'label' => 'Home',
+    ],
+    [
+        'route' => ['comics', 'comic'],
+        'label' => 'Comics',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'Movies',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'TV',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'Games',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'Collectibles',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'Videos',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'Fans',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'News',
+    ],
+    [
+        'route' => ['home'],
+        'label' => 'Shop',
+    ],
+];
---
+++ resources/views/guest/comics.blade.php
@@ -0,0 +1,5 @@
+@extends('guest.layouts.base')
+
+@section('content')
+    {{-- I have to print  product page --}}
+@endsection
