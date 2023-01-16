a972e9b3ecedf418f9f5b61c8aa808ba696a4967
LudwigEffort <dr.feynman65@gmail.com>
2023-01-16 01:19:50 +0100

    add foreach for comics

--- resources/views/guest/comics.blade.php
+++ resources/views/guest/comics.blade.php
@@ -2,4 +2,9 @@
 
 @section('content')
     {{-- I have to print  product page --}}
+    <ul>
+        @foreach ($comics as $comic)
+            <li>{{ $comic['title'] }}</li>
+        @endforeach
+    </ul>
 @endsection
